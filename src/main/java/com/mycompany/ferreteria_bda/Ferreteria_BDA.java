d0906fdf22fccf521ee143f0dbe131c649d5935b
chuyrincon <jesus.rincon206542@potros.itson.edu.mx>
2025-03-25 18:09:06 -0700

    aqui subi el repositorio

---
+++ src/main/java/com/mycompany/ferreteria_bda/Ferreteria_BDA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ferreteria_bda;
+
+/**
+ *
+ * @author Dell
+ */
+public class Ferreteria_BDA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
