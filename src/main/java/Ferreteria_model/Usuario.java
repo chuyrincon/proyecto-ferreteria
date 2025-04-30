c4165d8820fa469bd7203fc04114f9a67ea136d4
chuyrincon <jesus.rincon206542@potros.itson.edu.mx>
2025-04-29 19:53:02 -0700

    se creo el crud de provedores

---
+++ src/main/java/Ferreteria_model/Usuario.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ferreteria_model;
+
+/**
+ *
+ * @author Dell
+ */
+public class Usuario {
+      public Usuario(int id_usuario1, String nombre1, String correo1, String contraseña1, String rol1) {
+    }
+    private int id_usuario;
+    private String nombre;
+    private String correo;
+    private String password;
+    private String rol;
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public void add(Usuario usuarios) {
+        
+    }
+    
+}
